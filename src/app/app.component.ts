f40ed6cf2d67a4c069ad6401e6bf30331b1283e3
Hubert_podgorski <hubert.podgorski95@gmail.com>
2017-06-06 20:49:19 +0200

    pzekazuje dane z jednego do drugiego komponentu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  someString: string;
+  update(data) {
+    this.someString = data;
+  }
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { HttpService } from 'app/shared/http.service';
+import { ResultData } from './result-data';
 
 @Component({
   selector: 'app-search',
@@ -8,16 +9,25 @@ import { HttpService } from 'app/shared/http.service';
 })
 export class SearchComponent implements OnInit {
 
+  result: ResultData;
+  someString: string;
+
+  @Output() update = new EventEmitter();
+
   constructor(private httpService: HttpService) { }
 
   ngOnInit() {
     this.getData();
+    this.someString = 'some written in string to output';
   }
+
 
   getData() {
       this.httpService.getData('123123123').subscribe(
         data => {
+          this.result = data;
           console.log(data);
+          return this.result;
         }
       );
     }
